e67e350eec7d89fc3f22afe4cde70828ef0da6d0
greatlogistics <greatlogistics35@gmail.com>
2026-01-03 22:41:55 +0100

    IT WOULD WORK SENDTALK OFFICIAL

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,8 +1,16 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
 buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
     dependencies {
         classpath("com.android.tools.build:gradle:8.1.4")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.24")
     }
 }
-
-// No repositories block here!
--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,14 +1,4 @@
 pluginManagement {
-    val flutterSdkPath = run {
-        val properties = java.util.Properties()
-        file("local.properties").inputStream().use { properties.load(it) }
-        val path = properties.getProperty("flutter.sdk")
-        require(path != null) { "flutter.sdk not set in local.properties" }
-        path
-    }
-
-    includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
-
     repositories {
         google()
         mavenCentral()
