4edc7a11ce140ca5b072c1e79f2411a89770d9dd
greatlogistics <greatlogistics35@gmail.com>
2026-01-04 19:49:57 +0100

    BELIEVING THIS IS THE FINAL FIX

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -2,3 +2,16 @@ plugins {
     id("com.android.application") apply false
     id("org.jetbrains.kotlin.android") apply false
 }
+subprojects {
+    afterEvaluate {
+        val androidExtension = project.extensions.findByName("android")
+        if (androidExtension != null) {
+            val android = androidExtension as com.android.build.gradle.BaseExtension
+            // Check if namespace is missing
+            if (android.namespace == null) {
+                // Assign a namespace based on the project group/name
+                android.namespace = project.group.toString().ifEmpty { "com.example.${project.name.replace("-", "_")}" }
+            }
+        }
+    }
+}
