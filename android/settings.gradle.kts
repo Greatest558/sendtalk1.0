3f27eb9f05c4a28e82ad2c83c757ddc0046e62cd
greatlogistics <greatlogistics35@gmail.com>
2026-01-04 19:20:59 +0100

    Fix: Defined plugin versions in settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,18 +1,7 @@
-pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
+plugins {
+    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
+    // Add the version for the Android Application plugin here
+    id("com.android.application") version "8.13.0" apply false
+    // Add the version for the Kotlin plugin as well
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
 }
-
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
-rootProject.name = "sendtalk"
-include(":app")
